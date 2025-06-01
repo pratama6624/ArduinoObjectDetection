4478e1fe0b330ac71cf6906fed80a2e6f78bb06b
pratama6624 <scyoung6624@gmail.com>
2025-06-01 09:30:25 +0700

    Initial Commit

---
+++ ArduinoObjectDetection/ArduinoObjectDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArduinoObjectDetectionApp.swift
+//  ArduinoObjectDetection
+//
+//  Created by Pratama One on 01/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ArduinoObjectDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArduinoObjectDetection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArduinoObjectDetection
+//
+//  Created by Pratama One on 01/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
